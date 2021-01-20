81f01a824f74a49a0598d139c202efba90cbd3bc
Mohammed Usman <zad.designer@gmail.com>
2021-01-20 17:14:13 +0530

    Interpolation
    Angular interpolation is used display a component property in the respective view template with double curly braces syntax. ... Interpolation is used for one way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'usman4angular';
+  name = ' name is usman ';
+  work = 'today im learning interpolation';
+  test(){
+    return this.work
+  }
+  obj={
+    name:" Mohammed Usman",
+    age : '18',
+  }
+  arr=[
+    'anil' , ' zahed ' , 'hameed'
+  ]
+  a = 200 ;
+  b = 300 ;
+  siteUrl= window.location.href ; 
+  difination = " Angular interpolation is used display a component property in the respective view template with double curly braces syntax. ... Interpolation is used for one way data binding "
 }
